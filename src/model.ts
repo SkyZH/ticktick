8d36f200ed2c6787eb6b210b958105ce163fc468
SkyZH <iskyzh@gmail.com>
2018-08-27 22:44:59 +0800

    fix typescript compatibility

--- src/model.ts
+++ src/model.ts
@@ -1,6 +1,7 @@
-import Moment from 'moment';
-import _ from 'lodash';
-import { extendMoment, MomentRange } from 'moment-range';
+import * as Moment from 'moment';
+import * as _ from 'lodash';
+import { extendMoment } from 'moment-range';
+const moment = extendMoment(Moment);
 
 export interface IReport<T> {
   data: T;
@@ -21,27 +22,25 @@ const SUMMARY_FROM: { [unit: string]: Moment.unitOfTime.Base } = {
 };
 
 export class Model<T> {
-  private moment: Moment.Moment & MomentRange;
   constructor(
     private name: string,
     private summurizer: (a: T[]) => T,
     private querier: (table: string, id: string) => Promise<T>
-  ) {
-    this.moment = extendMoment(Moment);
-  }
+  ) {}
   public async report(
     data: T,
     forTime: Moment.Moment
   ): Promise<Array<IReport<T>>> {
+    const mForTime = moment(forTime);
     const secondData: IReport<T> = {
       data,
-      id: `${forTime.unix()}`,
+      id: `${mForTime.unix()}`,
       table: 'second',
     };
     return [secondData].concat(
       await Promise.all(
         _.map(TABLE_MAP, (table: Moment.unitOfTime.Base) =>
-          this.summary(table, secondData, forTime)
+          this.summary(table, secondData, mForTime)
         )
       )
     );
@@ -51,10 +50,10 @@ export class Model<T> {
     latest: IReport<T>,
     forTime: Moment.Moment
   ): Promise<IReport<T>> {
-    const start = this.moment(forTime).startOf(unit);
+    const start = moment(forTime).startOf(unit);
     const parentUnit = SUMMARY_FROM[unit] as Moment.unitOfTime.Base;
-    const current = this.moment(forTime).startOf(parentUnit);
-    const range = this.moment.range(start, current);
+    const current = moment(forTime).startOf(parentUnit);
+    const range = moment.range(start, current);
     const allDate = Array.from(range.by(parentUnit, { excludeEnd: true }));
     const allData = await Promise.all(
       allDate.map(d => this.querier(parentUnit, `${d.unix()}`))
